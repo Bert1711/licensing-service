eb4eaafc1c2e3b4ceab2c0767eb65869938f62a0
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-01-09 21:41:12 +0300

    "add LicenseController"

---
+++ src/main/java/com/zrn/license/controller/LicenseController.java
@@ -0,0 +1,12 @@
+package com.zrn.license.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Zaroyan
+ */
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+}
